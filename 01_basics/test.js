25e1da7329366e8e162f9ef2530e267177714e5e
Shubham Ghayal <96345308+Shubhamghayal@users.noreply.github.com>
2024-03-08 19:27:38 +0000

    day2

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,46 @@
-console.log("im testing")
+// console.log("im testing");
+// const accountId = 14453;
+// let accountEmail = "lsdnhovsd@gmail.com";
+// var accountPass = "35445";
+// accountCity = "lonar";
+// let accountState;
+
+// accountId = 212 //not allowed
+// accountEmail = "shubham@gmail.com";
+// accountPass = "354444";
+// accountCity = "lahor";
+// console.table([accountEmail,accountPass,accountCity,accountState]);
+/*
+prefer not to use var
+beccause of issue of block scope and functional scope
+*/
+
+
+// Datatypes and ECMA 
+"use strict" //treat all js code as newer version
+
+// alert() we are using node js
+/*number
+bigint
+string
+boolean
+null
+undefined
+symbol
+*/
+
+//object
+// console.log(typeof null)
+
+//conversion
+// "33">33
+// "33abc">NaN
+// true>1; false>0
+
+// OPerations //
+let value=3
+let negValue=-value
+console.log(negValue)
+
+
+
