165455ef0f55222fcc5ee1f082ee518127ba557d
Shubham Ghayal <96345308+Shubhamghayal@users.noreply.github.com>
2024-03-16 10:54:50 +0000

    changesimp

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -192,8 +192,45 @@ symbol
 // ice<=80?console.log("less than80"):console.log("greater than 80");
 
 //for loops
-let num=10;
-for(let i=0;i<=num;i++){
-    console.log(i);
+// let num=10;
+// for(let i=0;i<=num;i++){
+//     console.log(i);
     
-}
+// }
+// const arr=[1,2,3,4,5,6]
+// for (const num of arr) {
+//     console.log(num);
+    
+// }
+
+// const greetings="Hello world"
+// for (const greet of greetings) {
+// console.log(`each letter is${greet}`);
+    
+// }
+
+// const coding = ["js","ruby","python","cpp"]
+// coding.forEach( function (item){
+//     console.log(item)
+// })
+// coding.forEach((item)=>{console.log(item)}
+    
+// )
+
+const myCoding=[
+    {
+        name:"shubham",
+        language:"marathi"
+    },
+    {
+        name:"shu",
+        language:"hindi"
+    },
+    {
+        name:"manish",
+        language:"brgali"
+    }
+]
+myCoding.forEach((item)=>{
+    console.log(item.name,item.language)
+})
